808214de6417a81dec684281b8adbec9b150bc08
samyghost <d75w87ynpk@privaterelay.appleid.com>
2026-06-01 13:45:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("SamyGhost Pairing Server");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur le port ${PORT}`);
+});
