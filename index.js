14d0810c3ac0e386e7c221316351a104080008af
samyghost <d75w87ynpk@privaterelay.appleid.com>
2026-06-01 21:00:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,25 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.post("/pair", (req, res) => {
+    const { number } = req.body;
+
+    if (!number) {
+        return res.status(400).json({
+            message: "Veuillez entrer un numéro."
+        });
+    }
+
+    res.json({
+        message: `Numéro reçu : ${number}`
+    });
 });
 
 const PORT = process.env.PORT || 8080;
